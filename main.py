9a324f00410be1bdb53f833b25f4e24aaad040b7
Damian Galeano <dj.damian.b@hotmail.com>
2023-04-11 17:06:16 +0200

    call links

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+
+
+url = 'http://www.dailysmarty.com/topics/python'
+
+html_page = requests.get(url)
+# result = html_page.text
+
+soup = BeautifulSoup(html_page.text, 'html.parser')
+
+post_items = soup.find_all('a')
+
+
+for post in post_items:
+    print(post.get('href'))
